ff44831344feb13e814f710f470089fe32fa3a69
toshi hondo <toshi.hondo@qtplatz.com>
2021-07-27 23:54:36 +0000

    remove hard coded gpio number, that replaces with dt property read

--- src/drivers/pio/pio.c
+++ src/drivers/pio/pio.c
@@ -43,10 +43,6 @@
 #include <linux/platform_device.h>
 #include <linux/ktime.h>
 
-#if 0
-static int populate_device_tree( struct seq_file * m );
-#endif
-
 static char *devname = MODNAME;
 MODULE_AUTHOR( "Toshinobu Hondo" );
 MODULE_DESCRIPTION( "Device Driver for the Delay Pulse Generator" );
@@ -63,66 +59,22 @@ static struct cdev * __pio_cdev;
 static struct class * __pio_class;
 
 struct pio_driver {
-    uint32_t irq;
     uint64_t irqCount_;
     wait_queue_head_t queue;
     int queue_condition;
     struct semaphore sem;
-	struct gpio_desc *inject_in;
-    struct gpio_desc *inject_out;
-    struct gpio_descs * gpio_outs;
-    struct gpio_descs * gpio_ins;
-	struct gpio_desc *led;
-    struct gpio_desc *dipsw;
-    int legacy_inject_out;
-    int legacy_inject_in;
-    int legacy_led;
-    int legacy_dipsw;
-    int dipsw_irq;
-    int injin_irq;
-    void __iomem * iomem;
+	struct gpio_desc * inject_in;
+    struct gpio_desc * inject_out;
+	struct gpio_desc * led;
+    u32 gpio_inject_out; // legacy gpio number
+    u32 gpio_inject_in;  // legacy gpio number
+    u32 gpio_led;        // legacy gpio number
+    u32 injin_irq;
 };
 
 static struct semaphore __sem;
 static wait_queue_head_t __trig_queue;
 static int __trig_queue_condition = 0;
-
-// --> 2017-APR-24
-struct region {
-    struct resource * resource_;
-    void * iomem_;
-};
-
-struct region_allocated {
-    size_t size;
-    struct region regions[ 16 ];
-};
-
-struct gpio_register {
-    const char * name;
-    const char * gpio_name;
-    int gpio; // base
-    uint32_t count;
-    uint32_t regs[ 3 ];
-    uint32_t start;
-    volatile uint32_t * addr;
-};
-
-struct irq_allocated {
-    size_t size;
-    int irqNumber[ 128 ];
-};
-
-struct gpio_dev_id {
-    int gpioNumber;
-    int irqNumber;
-    struct gpio_register * parent;
-};
-
-struct gpio_allocated {
-    size_t size;
-    int gpioNumber[ 32 ];
-};
 
 struct trig_data {
     struct pio_trigger_data * cache_;
@@ -161,22 +113,19 @@ pio_proc_read( struct seq_file * m, void * v )
         seq_printf( m, "injin irq: %d\n",  drv->injin_irq );
         // seq_printf( m, "dipsw irq: %d\n",  drv->dipsw_irq );
 
-        int led = gpio_get_value( drv->legacy_led );
-        int inj_o = gpio_get_value( drv->legacy_inject_out );
-        int dipsw = 0;
-        for ( int i = 0; i < 4; ++i )
-            dipsw |= ( gpio_get_value( drv->legacy_dipsw + i ) ? 1 : 0 )<< i;
+        int led = gpio_get_value( drv->gpio_led );
+        int inj_o = gpio_get_value( drv->gpio_inject_out );
 
-        gpio_set_value( drv->legacy_inject_out, inj_o ? 0 : 1 );
-        gpio_set_value( drv->legacy_led, led ? 0 : 1 );
+        gpio_set_value( drv->gpio_inject_out, inj_o ? 0 : 1 );
+        gpio_set_value( drv->gpio_led, led ? 0 : 1 );
 
         int inj_i = 0;
         for ( int i = 0; i < 2; ++i )
-            inj_i |= ( gpio_get_value( drv->legacy_inject_in + i ) ? 1 : 0 ) << i;
+            inj_i |= ( gpio_get_value( drv->gpio_inject_in + i ) ? 1 : 0 ) << i;
 
         seq_printf( m
-                    , "led = %d, inect_out = %d, inj.in = 0x%x, dipsw=0x%x\n"
-                    , led, inj_o, inj_i, dipsw );
+                    , "led = %d, inect_out = %d, inj.in = 0x%x\n"
+                    , led, inj_o, inj_i );
     }
 
     return 0;
@@ -378,7 +327,8 @@ static int
 pio_perror( struct platform_device * pdev, const char * prefix, void * p )
 {
     if ( IS_ERR( p ) ) {
-        dev_info(&pdev->dev, "%s %p -- error code = %ld", prefix, p, PTR_ERR(p) );
+        if ( PTR_ERR( p ) != -EPROBE_DEFER )
+            dev_info(&pdev->dev, "%s error code: %ld", prefix, PTR_ERR(p) );
         return PTR_ERR(p);
     }
     return 0;
@@ -397,73 +347,63 @@ pio_module_probe( struct platform_device * pdev )
         platform_set_drvdata( pdev, drv );
     }
     struct pio_driver * drv = platform_get_drvdata( pdev );
+    struct device_node * node = pdev->dev.of_node;
 
     int rcode = 0;
-    drv->dipsw = devm_gpiod_get_optional( &pdev->dev, "dipsw", GPIOD_IN );
-    if ( pio_perror( pdev, "dipsw", drv->dipsw ) ) {
-        if ( ( rcode = devm_gpio_request( &pdev->dev, 1984, "dipsw" )) == 0 ) {
-            drv->legacy_dipsw = 1984;
-            gpio_direction_input( drv->legacy_dipsw );
-        } else {
-            dev_err( &pdev->dev, "legacy_dipsw request failed: %d", rcode );
-        }
-    }
 
+    // LED
     drv->led = devm_gpiod_get_optional( &pdev->dev, "led", GPIOD_OUT_LOW );
-    if ( pio_perror( pdev, "led", drv->led ) ) {
-        if ( ( rcode = devm_gpio_request( &pdev->dev, 2016, "led" ) ) == 0 ) {
-            drv->legacy_led = 2016;
-            gpio_direction_output( drv->legacy_led, 0 );
+    if ( pio_perror( pdev, "led", drv->led ) == -EPROBE_DEFER ) {
+        if ( of_property_read_u32_index( node, "led-gpios", 1, &drv->gpio_led ) == 0 ) {
+            dev_info(&__pdev->dev, "led-gpios switch to legacy api: numberr %d", drv->gpio_led );
+            if ( ( rcode = devm_gpio_request( &pdev->dev, 2016, "led" ) ) == 0 ) {
+                gpio_direction_output( drv->gpio_led, 0 );
+            }
         } else {
-            dev_err( &pdev->dev, "legacy_led request failed: %d", rcode );
+            dev_err( &pdev->dev, "legacy gpio led request failed: %d", rcode );
         }
     }
 
-    /* drv->gpio_outs = devm_gpiod_get_array( &pdev->dev, "cds_out", GPIOD_OUT_HIGH ); */
-    /* pio_perror( pdev, "gpio_outs", drv->gpio_outs ); */
-    drv->inject_out = devm_gpiod_get_optional( &pdev->dev, "inject_out", GPIOD_OUT_HIGH );
-    if ( pio_perror( pdev, "inject_out", drv->inject_out ) ) {
-        if ( ( rcode = devm_gpio_request( &pdev->dev, 1922, "inject_out" ) ) == 0 ) {
-            drv->legacy_inject_out = 1922;
-            gpio_direction_output( drv->legacy_inject_out, 1 );
-        } else {
-            dev_err( &pdev->dev, "legacy_inject_out request failed: %d", rcode );
-        }
-    }
-
+    // INJ.IN
     drv->inject_in = devm_gpiod_get_optional( &pdev->dev, "inject_in", GPIOD_IN );
-    if ( pio_perror( pdev, "inject_in", drv->inject_in ) ) {
-        if ( ( rcode = devm_gpio_request( &pdev->dev, 1888, "inject_in" ) ) == 0 ) {
-            drv->legacy_inject_in = 1888;
-            gpio_direction_input( drv->legacy_inject_in );
+    if ( pio_perror( pdev, "inject_in", drv->inject_in ) == -EPROBE_DEFER ) {
+        if ( of_property_read_u32_index( node, "inject_in-gpios", 1, &drv->gpio_inject_in ) == 0 ) {
+            dev_info(&__pdev->dev, "inject_in-gpios switch to legacy api: numberr %d", drv->gpio_inject_in );
+            if ( ( rcode = devm_gpio_request( &pdev->dev, drv->gpio_inject_in, "inject_in" ) ) == 0 ) {
+                gpio_direction_input( drv->gpio_inject_in );
+            }
         } else {
-            dev_err( &pdev->dev, "legacy_inject_in request failed: %d", rcode );
+            dev_err( &pdev->dev, "legacy gpio inject_in request failed: %d", rcode );
         }
     }
 
-    if ( drv->legacy_inject_in ) {
-        int irq;
-        if ( (irq = gpio_to_irq( drv->legacy_inject_in ) ) > 0 ) {
-            dev_info(&pdev->dev, "\tinj.in gpio irq is %d", irq );
-            if ( (rcode = devm_request_irq( &pdev->dev, irq, handle_interrupt
+    // INJ.OUT
+    drv->inject_out = devm_gpiod_get_optional( &pdev->dev, "inject_out", GPIOD_OUT_HIGH );
+    if ( pio_perror( pdev, "inject_out", drv->inject_out ) == -EPROBE_DEFER ) {
+        if ( of_property_read_u32_index( node, "inject_out-gpios", 1, &drv->gpio_inject_out ) == 0 ) {
+            dev_info(&__pdev->dev, "inject_out-gpios switch to legacy api: numberr %d", drv->gpio_inject_out );
+            if ( ( rcode = devm_gpio_request( &pdev->dev, drv->gpio_inject_out, "inject_out" ) ) == 0 ) {
+                gpio_direction_output( drv->gpio_inject_out, 1 );
+            }
+        } else {
+            dev_err( &pdev->dev, "legacy gpio inject_out request failed: %d", rcode );
+        }
+    }
+
+    // IRQ
+    if ( gpio_is_valid( drv->gpio_inject_in ) ) {
+        if ( (drv->injin_irq = gpio_to_irq( drv->gpio_inject_in ) ) > 0 ) {
+            dev_info(&pdev->dev, "\tinj.in gpio irq is %d", drv->injin_irq );
+            if ( (rcode = devm_request_irq( &pdev->dev
+                                            , drv->injin_irq
+                                            , handle_interrupt
                                             , IRQF_SHARED | IRQF_TRIGGER_FALLING
-                                            , "inj.in", pdev )) == 0 ) {
-                drv->injin_irq = irq;
-            } else {
+                                            , "inj.in"
+                                            , pdev )) != 0 ) {
                 dev_err( &pdev->dev, "inj.in irq request failed: %d", rcode );
             }
         }
     }
-#if 0
-    if ( (irq = gpio_to_irq( drv->legacy_dipsw ) ) > 0 ) {
-        dev_info(&pdev->dev, "\tdipsw gpio irq is %d", irq );
-        if ( (rcode = devm_request_irq( &pdev->dev, irq, handle_interrupt, 0, "dipsw", pdev )) == 0 ) {
-            drv->dipsw_irq = irq;
-        } else {
-            dev_err( &pdev->dev, "dipsw irq request failed: %d", rcode );
-        }
-    }
-#endif
     return 0;
 }
 
@@ -499,58 +439,3 @@ static struct platform_driver __platform_driver = {
 
 module_init( pio_module_init );
 module_exit( pio_module_exit );
-
-#if 0
-static int print_device_node( struct seq_file * m, struct device_node * child, const char * prefix )
-{
-    seq_printf( m, "%schild->name = %s, addr_cells = %d, size_cells = %d\n"
-                , prefix
-                , child->name
-                , of_n_addr_cells( child )
-                , of_n_size_cells( child ) );
-    struct property * prop = child->properties;
-    do {
-        const char * value[ 32 ] = { 0 };
-        u32 ivalue[ 32 ] = { 0 };
-        seq_printf( m, "%s\tname = %s\t", prefix, prop->name );
-
-        if ( of_property_read_string( child, prop->name, value ) == 0 ) {
-            for ( int i = 0; i < countof(value) && value[ i ]; ++i )
-                seq_printf( m, "'%s' ", value[i] );
-
-            if ( strlen( value[0] ) == 0 ) {
-                int sz = prop->length / 4 < countof(ivalue) ? prop->length / 4 : countof(ivalue);
-                if ( of_property_read_u32_array( child, prop->name, ivalue, sz ) == 0 ) {
-                    for ( int i = 0; i < prop->length / 4; ++i )
-                        seq_printf( m, "0x%x ", ivalue[ i ] );
-                }
-            }
-        }
-        seq_printf( m, "\n" );
-    } while (( prop = prop->next ));
-
-    return 0;
-}
-#endif
-
-#if 0
-static int populate_device_tree( struct seq_file * m )
-{
-    struct device_node * node;
-    struct device_node * child;
-
-    // trial to find hps_led0
-    if ( ( node = of_find_node_by_path( "/soc/base_fpga_region" ) ) ) {
-
-        for_each_child_of_node( node, child ) {
-            print_device_node( m, child, "" );
-
-            struct device_node * c1;
-            for_each_child_of_node( child, c1 )
-                print_device_node( m, c1, "\t" );
-        }
-        return 0;
-    }
-    return -1;
-}
-#endif

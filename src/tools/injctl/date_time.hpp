0ca474b838aa2ea5362ea765efccbe37ee1320a5
Toshinobu Hondo <toshi.hondo@qtplatz.com>
2021-07-28 14:56:09 +0900

    add injection event test code

---
+++ src/tools/injctl/date_time.hpp
@@ -0,0 +1,102 @@
+/**************************************************************************
+** Copyright (C) 2020 MS-Cheminformatics LLC
+*
+** Contact: info@ms-cheminfo.com
+**
+** Commercial Usage
+**
+** Licensees holding valid MS-Cheminformatics commercial licenses may use this file in
+** accordance with the MS-Cheminformatics Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and MS-Cheminformatics.
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.TXT included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**************************************************************************/
+
+#pragma once
+
+// #include "adportable_global.h"
+#include <date/date.h>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/format.hpp>
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+namespace adportable {
+
+    struct /* ADPORTABLESHARED_EXPORT */ subseconds {
+        static std::string to_str( int64_t, size_t n );
+    };
+
+    template< bool is_system_clock = true >
+    struct date_time_t {
+        template< typename duration_t, typename time_point_t >
+        std::pair< std::time_t, duration_t > to_time_t( time_point_t tp ) {
+            // std::cout << "-- system_clock -->";
+            auto utc = std::chrono::system_clock::to_time_t( std::chrono::time_point_cast< std::chrono::system_clock::duration >( tp ) );
+            return { utc, duration_t( std::chrono::time_point_cast< duration_t >( tp ) - date::floor< std::chrono::seconds >( tp ) ) };
+        }
+    };
+
+    template<>
+    template< typename duration_t, typename time_point_t >
+    std::pair< std::time_t, duration_t >
+    date_time_t< false >::to_time_t( time_point_t tp ) {
+        typedef typename decltype( tp )::clock clock_t;
+        // std::cout << "-- steady_clock -->";
+        auto tt = std::chrono::time_point_cast< duration_t >( std::chrono::system_clock::now() ) + ( tp - clock_t::now() );
+        auto utc = std::chrono::system_clock::to_time_t( std::chrono::time_point_cast< std::chrono::system_clock::duration >( tt ) );
+        return { utc, std::chrono::duration_cast< duration_t >( tt - date::floor< std::chrono::seconds >( tt ) ) };
+    }
+
+    template< uint64_t N > struct num_digits {
+        enum { value = 1 + num_digits< N / 10 >::value };
+    };
+
+    template<> struct num_digits<0> {
+        enum { value = 0 };
+    };
+
+    struct date_time {
+
+        template< typename duration_t, typename time_point_t >
+        static std::string to_iso( time_point_t tp, bool utc_offset = true ) {
+            typedef typename decltype( tp )::clock clock_t;
+#if __cplusplus < 201703L
+            std::time_t utc; duration_t subseconds;
+            std::tie( utc, subseconds )
+#else
+            auto [utc, subseconds]
+#endif
+                  = date_time_t< std::is_same< clock_t, std::chrono::system_clock >::value >(). template to_time_t< duration_t >( tp );
+
+            // subseconds_t< num_digits< duration_t::period::den >::value - 1 > subseconds_to_string;
+            constexpr size_t N = num_digits< duration_t::period::den >::value - 1;
+
+            std::ostringstream o;
+            if ( utc_offset ) {
+                auto tz( boost::posix_time::second_clock::local_time() - boost::posix_time::second_clock::universal_time() );
+                o << std::put_time( std::localtime(&utc), "%FT%T" ) << subseconds::to_str( subseconds.count(), N )
+                  << boost::format( "%c%02d%02d" )
+                    % (tz.is_negative() ? '-' : '+')
+                    % boost::date_time::absolute_value( tz.hours() )
+                    % boost::date_time::absolute_value( tz.minutes() );
+            } else {
+                auto syst = std::chrono::time_point_cast< duration_t >( std::chrono::system_clock::from_time_t( utc ) ) + duration_t( subseconds );
+                o << date::format("%FT%TZ", syst );
+            }
+            return o.str();
+        }
+    };
+}
---
+++ src/tools/injctl/main.cpp
@@ -0,0 +1,85 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Toshinobu Hondo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "date_time.hpp"
+#include <boost/program_options.hpp>
+#include <boost/format.hpp>
+#include <array>
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <memory>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h> // close
+
+bool __verbose = true;
+
+int
+main( int argc, char **argv )
+{
+    namespace po = boost::program_options;
+    po::variables_map vm;
+    po::options_description description( argv[ 0 ] );
+    {
+        description.add_options()
+            ( "help,h",        "Display this help message" )
+            ( "device,d",      po::value< std::string >()->default_value("/dev/pio0"), "pio device" )
+            ( "wait",          "wait for injection" )
+            ( "replicates,r",  po::value< uint32_t >()->default_value(1), "replicates for inject wait" )
+            ;
+        po::positional_options_description p;
+        p.add( "args",  -1 );
+        po::store( po::command_line_parser( argc, argv ).options( description ).positional(p).run(), vm );
+        po::notify(vm);
+    }
+
+    if ( vm.count( "help" ) ) {
+        std::cerr << description;
+        return 0;
+    }
+
+    size_t replicates = vm[ "replicates" ].as< uint32_t >();
+
+    if ( vm.count( "wait" ) ) {
+        uint64_t rep[ 2 ];
+        int fd = ::open( vm[ "device" ].as< std::string >().c_str(), O_RDONLY );
+        if ( fd > 0 ) {
+            while ( replicates-- ) {
+                std::cerr  << "waiting..." << std::endl;
+                auto count = ::read( fd, &rep, sizeof( rep ) );
+                std::cout << "read count: " << count << ", rep: " << rep[0] << ", " << rep[1] << std::endl;
+                auto dur = std::chrono::nanoseconds( rep[ 0 ] );
+                auto tp = std::chrono::time_point< std::chrono::system_clock >( dur );
+                std::cout << adportable::date_time::to_iso< std::chrono::microseconds >( tp, true ) << std::endl;
+            }
+        }
+    }
+    return 0;
+}
